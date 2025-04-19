f0d60a117ef9d5e78ec7f8bf3ae3a9121fe10fad
borisey <51847223+borisey@users.noreply.github.com>
2025-04-19 20:49:10 +0300

    user

---
+++ src/main/java/com/example/otp_security_service/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.example.otp_security_service.controllers;
+
+import com.example.otp_security_service.models.User;
+import com.example.otp_security_service.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+}
